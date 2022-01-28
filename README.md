fa31fd33849b506a7cc177fada4feb9f04fc7569
Kid R <rainui.ly@gmail.com>
2022-01-28 12:03:28 +0100

    new: First release of commitlinter as GitHub Action
    [LINT-4]
    - Adapt index.js, change tag and fixes
    - Rework of commitlinter to be independant of config.json
    - fix: adapt tests because of the rework
    - tests OK

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# commitlinter
-Personal git commit linter for GitHub action
+# Rxinui/commitlinter
+
+Personal git commit linter for GitHub action following [Rxinui commit format](https://kidr.atlassian.net/wiki/spaces/PFEDOJOPLA/pages/229378/How-to+write+a+valid+commit)
+
+## Usage on GitHub Action
+
+```yaml
+on: [push]
+jobs:
+  commit_linter_job:
+    runs-on: ubuntu-latest
+    name: A job to lint the current commit of the branch
+    steps:
+      - name: Commit Linter Action
+        id: lintResult
+        uses: Rxinui/commitlinter@v1.0
+        with:
+          commitTags: "new,fix,doc,ref,wip" # required - specify valid tags, separed by commas
+          commitIssueId: "LINT" # required - specify the issue ID (eg. Jira issue id tracker)
+```
--- spec/simpleSpec.js
+++ spec/simpleSpec.js
@@ -1,5 +1,13 @@
 const commitlint = require('../commitlint');
 
+const commitTags = [
+    "new",
+    "fix",
+    "doc",
+    "ref",
+    "wip"
+];
+const commitIssueId = "PFE";
 const testcase =
     `new: VBoxManageList commands work with API
 
@@ -15,29 +23,29 @@ const testcase =
 
 describe("Test lint headline:", function () {
     it("Headline starts with a valid tag", function () {
-        expect(commitlint.lintHeadline(testcase).codes).toContain(commitlint.PASSED);
+        expect(commitlint.lintHeadline(testcase, commitTags, commitIssueId).codes).toContain(commitlint.PASSED);
     });
 
     it("Bad: Headline does not start with a tag", () => {
         const badTestCase = `VBoxManageList commands work with API`.split("\n");
-        expect(commitlint.lintHeadline(badTestCase).codes).toContain(commitlint.ERROR_MISSING_TAG);
-        expect(commitlint.lintHeadline(badTestCase).codes).toContain(commitlint.ERROR_INCORRECT_TAG);
-        expect(commitlint.lintHeadline(badTestCase).codes).toContain(commitlint.ERROR_MISSING_TAG_WHITESPACE);
+        expect(commitlint.lintHeadline(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_MISSING_TAG);
+        expect(commitlint.lintHeadline(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_INCORRECT_TAG);
+        expect(commitlint.lintHeadline(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_MISSING_TAG_WHITESPACE);
     })
 
     it("Bad: Headline tag is not followed by a whitespace", () => {
         const badTestCase = `new:VBoxManageList commands work with API`.split("\n");
-        expect(commitlint.lintHeadline(badTestCase).codes).toContain(commitlint.ERROR_MISSING_TAG_WHITESPACE);
+        expect(commitlint.lintHeadline(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_MISSING_TAG_WHITESPACE);
     })
 
     it("Bad: Headline tag length is less than 3 characters", () => {
         const badTestCase = `wp: VBoxManageList commands work with API`.split("\n");
-        expect(commitlint.lintHeadline(badTestCase).codes).toContain(commitlint.ERROR_INCORRECT_TAG);
+        expect(commitlint.lintHeadline(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_INCORRECT_TAG);
     })
 
     it("Bad: Headline tag length is less than 3 characters", () => {
         const badTestCase = `ref: `.split("\n");
-        expect(commitlint.lintHeadline(badTestCase).codes).toContain(commitlint.ERROR_MISSING_HEADLINE_SUMMARY);
+        expect(commitlint.lintHeadline(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_MISSING_HEADLINE_SUMMARY);
     })
 
 });
@@ -45,7 +53,7 @@ describe("Test lint headline:", function () {
 
 describe("Test lint headline newline:", function () {
     it("Valid headline newline", function () {
-        expect(commitlint.lintHeadlineNewline(testcase).codes).toContain(commitlint.PASSED);
+        expect(commitlint.lintHeadlineNewline(testcase, commitTags, commitIssueId).codes).toContain(commitlint.PASSED);
     });
 
     it("Bad: No newline between headline and issue number", () => {
@@ -54,14 +62,14 @@ describe("Test lint headline newline:", function () {
 - VBoxManage list command is run with subprocess
 - parse and display list vms OK
 `.split("\n");
-        expect(commitlint.lintHeadlineNewline(badTestCase).codes).toContain(commitlint.ERROR_MISSING_HEADLINE_NEWLINE);
+        expect(commitlint.lintHeadlineNewline(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_MISSING_HEADLINE_NEWLINE);
     })
 });
 
 
 describe("Test lint issue number:", function () {
     it("Valid issue number", function () {
-        expect(commitlint.lintIssueNumber(testcase).codes).toContain(commitlint.PASSED);
+        expect(commitlint.lintIssueNumber(testcase, commitTags, commitIssueId).codes).toContain(commitlint.PASSED);
     });
 
     it("Bad: Missing issue number", function () {
@@ -69,11 +77,11 @@ describe("Test lint issue number:", function () {
             `new: VBoxManageList commands work with API
 
 `.split("\n");
-        expect(commitlint.lintIssueNumber(badTestCase).codes).toContain(commitlint.ERROR_MISSING_ISSUE);
-        expect(commitlint.lintIssueNumber(badTestCase).codes).toContain(commitlint.ERROR_MISSING_ISSUE_OPEN_BRACKET);
-        expect(commitlint.lintIssueNumber(badTestCase).codes).toContain(commitlint.ERROR_INCORRECT_ISSUE_ID);
-        expect(commitlint.lintIssueNumber(badTestCase).codes).toContain(commitlint.ERROR_MISSING_ISSUE_NUMBER);
-        expect(commitlint.lintIssueNumber(badTestCase).codes).toContain(commitlint.ERROR_MISSING_ISSUE_CLOSE_BRACKET);
+        expect(commitlint.lintIssueNumber(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_MISSING_ISSUE);
+        expect(commitlint.lintIssueNumber(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_MISSING_ISSUE_OPEN_BRACKET);
+        expect(commitlint.lintIssueNumber(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_INCORRECT_ISSUE_ID);
+        expect(commitlint.lintIssueNumber(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_MISSING_ISSUE_NUMBER);
+        expect(commitlint.lintIssueNumber(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_MISSING_ISSUE_CLOSE_BRACKET);
     });
 
     it("Bad: Missing '[' bracket issue number", function () {
@@ -81,7 +89,7 @@ describe("Test lint issue number:", function () {
             `new: VBoxManageList commands work with API
 
 PFE-4]`.split("\n");
-        expect(commitlint.lintIssueNumber(badTestCase).codes).toContain(commitlint.ERROR_MISSING_ISSUE_OPEN_BRACKET);
+        expect(commitlint.lintIssueNumber(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_MISSING_ISSUE_OPEN_BRACKET);
     });
 
     it("Bad: Incorrect issue id", function () {
@@ -89,8 +97,8 @@ PFE-4]`.split("\n");
             `new: VBoxManageList commands work with API
 
 [WRONG-4`.split("\n");
-        expect(commitlint.lintIssueNumber(badTestCase).codes).toContain(commitlint.ERROR_INCORRECT_ISSUE_ID);
-        expect(commitlint.lintIssueNumber(badTestCase).codes).toContain(commitlint.ERROR_MISSING_ISSUE_CLOSE_BRACKET);
+        expect(commitlint.lintIssueNumber(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_INCORRECT_ISSUE_ID);
+        expect(commitlint.lintIssueNumber(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_MISSING_ISSUE_CLOSE_BRACKET);
     });
 
     it("Bad: Missing issue number", function () {
@@ -98,7 +106,7 @@ PFE-4]`.split("\n");
             `new: VBoxManageList commands work with API
 
 [PFE-]`.split("\n");
-        expect(commitlint.lintIssueNumber(badTestCase).codes).toContain(commitlint.ERROR_MISSING_ISSUE_NUMBER);
+        expect(commitlint.lintIssueNumber(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_MISSING_ISSUE_NUMBER);
     });
 
     it("Bad: Missing ']' bracket issue number", function () {
@@ -106,13 +114,13 @@ PFE-4]`.split("\n");
             `new: VBoxManageList commands work with API
 
 [PFE-4`.split("\n");
-        expect(commitlint.lintIssueNumber(badTestCase).codes).toContain(commitlint.ERROR_MISSING_ISSUE_CLOSE_BRACKET);
+        expect(commitlint.lintIssueNumber(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_MISSING_ISSUE_CLOSE_BRACKET);
     });
 });
 
 describe("Test lint description of essential changes:", function () {
     it("Valid description", function () {
-        expect(commitlint.lintDescriptionBody(testcase).codes).toContain(commitlint.PASSED);
+        expect(commitlint.lintDescriptionBody(testcase, commitTags, commitIssueId).codes).toContain(commitlint.PASSED);
     });
 
     it("Bad: Description with whitespaces only", function () {
@@ -121,7 +129,7 @@ describe("Test lint description of essential changes:", function () {
 
 [PFE-4]
        `.split("\n")
-        expect(commitlint.lintDescriptionBody(badTestCase).codes).toContain(commitlint.ERROR_MISSING_DESCRIPTION);
+        expect(commitlint.lintDescriptionBody(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_MISSING_DESCRIPTION);
     });
 
     it("Bad: Description with newline only", function () {
@@ -133,7 +141,7 @@ describe("Test lint description of essential changes:", function () {
 
 
 `.split("\n")
-        expect(commitlint.lintDescriptionBody(badTestCase).codes).toContain(commitlint.ERROR_MISSING_DESCRIPTION);
+        expect(commitlint.lintDescriptionBody(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_MISSING_DESCRIPTION);
     });
 
     it("Bad: Description with newline and whitespaces only", function () {
@@ -145,6 +153,6 @@ describe("Test lint description of essential changes:", function () {
            
    
 `.split("\n")
-        expect(commitlint.lintDescriptionBody(badTestCase).codes).toContain(commitlint.ERROR_MISSING_DESCRIPTION);
+        expect(commitlint.lintDescriptionBody(badTestCase, commitTags, commitIssueId).codes).toContain(commitlint.ERROR_MISSING_DESCRIPTION);
     });
 });
